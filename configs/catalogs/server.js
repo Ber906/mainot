2192a892805b6fab8ed4d65c62807ccb527c94b4
Ainz <143864591+Ber906@users.noreply.github.com>
2023-12-28 16:51:34 +0800

    Update server.js

--- configs/catalogs/server.js
+++ configs/catalogs/server.js
@@ -7,8 +7,6 @@ const logger = require("./system-settings/console/console-logger.js");
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static('public'));
-
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
