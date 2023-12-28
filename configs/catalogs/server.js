ab7deb9af795301e9300a08e7533cf625cfc4ce6
Ainz <143864591+Ber906@users.noreply.github.com>
2023-12-28 21:01:37 +0800

    Update server.js

--- configs/catalogs/server.js
+++ configs/catalogs/server.js
@@ -1,91 +1,35 @@
+console.clear();
 const { spawn } = require("child_process");
 const express = require("express");
 const app = express();
+const chalk = require('chalk');
 const logger = require("./system-settings/console/console-logger.js");
 const path = require('path');
-const net = require('net');
-const chalk = require('chalk');
-const pkg = require('../../package.json');
-const axios = require('axios');
-
-const getRandomPort = () => Math.floor(Math.random() * (65535 - 1024) + 1024);
-const PORT = getRandomPort();
-let currentPort = PORT;
-const REPL_HOME = `https://ainz-dev.onrender.com`.toLowerCase();
-
+const PORT = process.env.PORT || 10000;
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/public/index.html'));
 });
-
-app.get('/', (req, res) => res.sendStatus(200));
-
-setInterval(uptime, 1000);
-
-function uptime() {
-  axios.get(REPL_HOME).then(() => {}).catch(() => {});
-}
-
-  console.clear();
-  console.log(chalk.bold.dim(`ainz-dev`.toUpperCase() + `(v${pkg.version})`));
-  logger(`Getting Started!`, "STARTER");
-  startBot(0);
-
-  async function isPortAvailable(port) {
-    return new Promise((resolve) => {
-      const tester = net.createServer()
-        .once('error', () => resolve(false))
-        .once('listening', () => {
-          tester.once('close', () => resolve(true)).close();
-        })
-        .listen(port, '127.0.0.1');
-    });
-  }
-
-  function startServer(port) {
-    app.listen(port, () => {
-      logger.loader(`Bot is running on port: ${port}`);
-      logger.loader(`Activating uptime for ${chalk.underline(`'${REPL_HOME}'`)}`, 'SYSTEM');
-    });
-
-    app.on('error', (error) => {
-      logger(`An error occurred while starting the server: ${error}`, "SYSTEM");
-    });
-  }
-
-// # Please note that sometimes this function is the reason the bot will auto-restart, even if your custom.js auto-restart is set to false. This is because the port switches automatically if it is unable to connect to the current port. ↓↓↓↓↓↓
-
-  async function startBot(index) {
-    try {
-      const isAvailable = await isPortAvailable(currentPort);
-      if (!isAvailable) {
-        logger(`Retrying...`, "SYSTEM");
-        const newPort = getRandomPort();
-        logger.loader(`Current port ${currentPort} is not available. Switching to new port ${newPort}.`);
-        currentPort = newPort;
-      }
-      
-      startServer(currentPort);
-
-      const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "system.js"], {
+console.clear();
+function startBot(message) {
+    (message) ? logger(message, "starting") : "";
+  console.log(chalk.blue('𝙳𝙴𝙿𝙻𝙾𝚈𝙸𝙽𝙶 𝙼𝙰𝙸𝙽 𝚂𝙴𝚁𝚅𝙴𝚁\n'));
+  logger.loader(`deploying app on port ${chalk.blueBright(PORT)}`);
+  app.listen(logger.loader(`app deployed on port ${chalk.blueBright(PORT)}`));
+  const child = spawn("node", ["--trace-warnings", "--async-stack-traces", "system.js"], {
         cwd: __dirname,
         stdio: "inherit",
-        shell: true,
-        env: {
-          ...process.env,
-          CHILD_INDEX: index,
-        },
-      });
+        shell: true
+    });
+  child.on("close", (codeExit) => {
+        if (codeExit != 0 || global.countRestart && global.countRestart < 5) {
+            startBot();
+            global.countRestart += 1;
+            return;
+        } else return;
+    });
 
-      child.on("close", (codeExit) => {
-        if (codeExit !== 0) {
-          startBot(index);
-        }
-      });
-
-      child.on("error", (error) => {
-        logger(`An error occurred while starting the child process: ${error}`, "SYSTEM");
-      });
-    } catch (err) {
-      logger(`Error while starting the bot: ${err}`, "SYSTEM");
-    }
-  }
+  child.on("error", function(error) {
+    logger("an error occurred : " + JSON.stringify(error), "error");
+  });
+};
+startBot();
